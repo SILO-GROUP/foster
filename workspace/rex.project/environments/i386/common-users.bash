e6f39f2af242857abf3f4dcfd09521fda089f316
Chris Punches <chris.punches@silogroup.org>
2021-08-08 18:37:50 -0400

    removed variant b artifacts

--- workspace/rex.project/environments/i386/common-users.bash
+++
@@ -1,6 +0,0 @@
-# builder user and group settings
-#
-# These values are used for things that need done by a non-root user
-BUILD_GROUP=royalty
-BUILD_USER=phanes
-
--- workspace/rex.project/environments/i386/crosstool-ng_gen_0.bash
+++
@@ -1,6 +0,0 @@
-# Cross-compiler generation environment file.
-
-# load common functions and directory variables into environment
-source environments/common-functions.bash 
-source environments/common-directories.bash
-
